0bbd1380614a484617ccee12b598b91e7042b43c
CodesNiAyban <101535798+CodesNiAyban@users.noreply.github.com>
2024-06-07 01:51:11 +0800

    Production Fix

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,24 +1,27 @@
 import { Api, StackContext, use } from "sst/constructs";
 import { StorageStack } from "./StorageStack";
 
-export function ApiStack({ stack }: StackContext) {
+export function ApiStack({ stack, app }: StackContext) {
   const { table } = use(StorageStack);
+
+  
 
   // Create the API
   const api = new Api(stack, "Api", {
+    customDomain: app.stage === "prod" ? "ssttest.com" : undefined,
     defaults: {
       function: {
         bind: [table],
       },
     },
     routes: {
-      "POST /notes": "packages/functions/src/create.main",
+      "POST /notes": "packages/functions/src/create.main",  
     },
   });
 
   // Show the API endpoint in the output
   stack.addOutputs({
-    ApiEndpoint: api.url,
+    ApiEndpoint: api.customDomainUrl || api.url,
   });
 
   // Return the API resource
